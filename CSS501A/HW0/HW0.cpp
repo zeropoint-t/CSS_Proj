13fb8f1b13e274979bbf8de8bed6e125ec176274
zeropoint-t <tetsuyaeinstein@gmail.com>
2018-09-29 11:08:05 -0700

    Test Commit

--- CSS501A/HW0/HW0.cpp
+++ CSS501A/HW0/HW0.cpp
@@ -1,6 +1,6 @@
 
 /*
-I want a technology to allow my mind to think of codes and type on a screen. This way, we programmers can speed up our train of thoughts.
+I want a technology to allow my mind to think of codes and be able to type on a screen. This way, we programmers can speed up our train of thoughts.
 */
 
 #include <iostream>
