3a40cda7bdd8a9539ddc9067e1422258eeb93a76
zeropoint-t <tetsuyaeinstein@gmail.com>
2018-10-02 07:04:07 -0700

    commit 10-2-2018

---
+++ CSS501A/InClassExercise/School.h
@@ -0,0 +1,23 @@
+
+#ifndef School_h
+#define School_h
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class School{
+private:
+    string name;
+    string address;
+    int numOfFaculty;
+    int numOfSchool;
+    string type;
+        
+public:
+    void RegisterClass();
+
+};
+
+#endif
