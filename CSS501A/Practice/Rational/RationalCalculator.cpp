462959726c1674914f69c8ec25ea6bdbe5fc6bd5
Tetsuya Hayashi (Teleion Consulting LLC) <v-thaya@microsoft.com>
2018-10-04 09:03:22 -0700

    commit 10-04-2018

--- CSS501A/Practice/Rational/RationalCalculator.cpp
+++ CSS501A/Practice/Rational/RationalCalculator.cpp
@@ -8,88 +8,88 @@
 using namespace std;
 
 
-class Check
-{
-  friend ostream& operator<<(ostream &outStream, const Check &check);
+// class Check
+// {
+//   friend ostream& operator<<(ostream &outStream, const Check &check);
 
-  private:
-    int i;
-  public:
-    Check(): i(0) {  }
-    Check operator ++ ()
-    {
-        Check temp;
-        temp.i = ++i;
-        return temp;
-    }
+//   private:
+//     int i;
+//   public:
+//     Check(): i(0) {  }
+//     Check operator ++ ()
+//     {
+//         Check temp;
+//         temp.i = ++i;
+//         return temp;
+//     }
 
-    // Notice int inside barcket which indicates postfix increment.
-    Check operator ++ (int)
-    {
-        Check temp;
-        temp.i = i++;
-        return temp;
-    }
+//     // Notice int inside barcket which indicates postfix increment.
+//     Check operator ++ (int)
+//     {
+//         Check temp;
+//         temp.i = i++;
+//         return temp;
+//     }
 
-    void Display()
-    { cout << "i = "<< i <<endl; }
-};
+//     void Display()
+//     { cout << "i = "<< i <<endl; }
+// };
 
-ostream& operator<<(ostream &outStream, const Check &check)
-{
-	outStream << check.i;
-	return outStream;
-}
+// ostream& operator<<(ostream &outStream, const Check &check)
+// {
+// 	outStream << check.i;
+// 	return outStream;
+// }
 
-ostream& operator<<(ostream &outStream, const Check &check)
-{
-	outStream << check.i;
-	return outStream;
-}
+// ostream& operator<<(ostream &outStream, const Check &check)
+// {
+// 	outStream << check.i;
+// 	return outStream;
+// }
 
 int main()
 {
-    Check obj;    
-    obj.Display(); 
-    obj++;
-    ++obj;
+    // Check obj;    
+    // obj.Display(); 
+    // obj++;
+    // ++obj;
 
-    cout << obj << endl;
+    // cout << obj << endl;
 
 	int waitVar;
-	// Rational rat1(3, 7), rat2(5, 10);
-	// Rational rat3, rat4, rat5, rat6;
-	// cout << "Welcome to our calculator" << endl;
+	Rational rat1(3, 7), rat2(5, 10);
+	Rational rat3, rat4, rat5, rat6;
+	cout << "Welcome to our calculator" << endl;
 
-    // cout << rat1 << endl;
+    cout << rat1 << endl;
 
-	// cout << "rat1: " << rat1 << endl;	
-	// cout << "rat2: " << rat2 << endl;
-	// cout << "rat3: " << rat3 << endl;
+	cout << "rat1: " << rat1 << endl;	
+	cout << "rat2: " << rat2 << endl;
+	cout << "rat3: " << rat3 << endl;
 
-	// cout << "rat1 + rat2 = " << (rat1 + rat2) << endl;
-	// cout << "rat1 - rat2 = " << (rat1 - rat2) << endl;
-	// cout << "rat1 * rat2 = " << (rat1 * rat2) << endl;
-	// cout << "rat1 / rat2 = " << (rat1 / rat2) << endl;
-	// cout << "-rat1 = " << -rat1 << endl;
+	cout << "rat1 + rat2 = " << (rat1 + rat2) << endl;
+	cout << "rat1 - rat2 = " << (rat1 - rat2) << endl;
+	cout << "rat1 * rat2 = " << (rat1 * rat2) << endl;
+	cout << "rat1 / rat2 = " << (rat1 / rat2) << endl;
+	cout << "-rat1 = " << -rat1 << endl;
 
-	// rat1 -= rat2;
-	// cout << "rat1 -= rat2; rat1: " << rat1 << endl;
+	rat1 -= rat2;
+	cout << "rat1 -= rat2; rat1: " << rat1 << endl;
 
 
 
-	// cout << "Input a rational. Numerator and denomiator as two integers: ";
-	// cin >> rat3;
-	// cout << "You entered: " << rat3 << endl;
+	cout << "Input a rational. Numerator and denomiator as two integers: ";
+	cin >> rat3;
+	cout << "You entered: " << rat3 << endl;
 
-	// if (rat3 == rat2)
-	// {
-	// 	cout << "The rational you entered is equal to rat2" << endl;
-	// }
-	// else
-	// {
-	// 	cout << "The rational you entered is not equal to rat2" << endl;
-	// }
+	if (rat3 == rat2)
+	{
+		cout << "The rational you entered is equal to rat2" << endl;
+	}
+	else
+	{
+		cout << "The rational you entered is not equal to rat2" << endl;
+	}
 
 	return 0;
 }
