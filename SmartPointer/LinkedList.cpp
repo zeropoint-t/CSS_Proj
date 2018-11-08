4210f4e21c1154fbf1ca7953f7eba1d53090014d
zeropoint-t <tetsuyaeinstein@gmail.com>
2018-11-08 07:04:07 -0800

    directory cleaned up

--- SmartPointer/LinkedList.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  LinkedList.cpp
-//  SmartPointer
-//
-//  Created by Tetsuya Hayashi on 9/19/18.
-//  Copyright © 2018 Tetsuya Hayashi. All rights reserved.
-//
-
-#include "LinkedList.hpp"
--- SmartPointer/SmartPointer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SmartPointer.cpp
-//  SmartPointer
-//
-//  Created by Tetsuya Hayashi on 9/19/18.
-//  Copyright © 2018 Tetsuya Hayashi. All rights reserved.
-//
-
-#include "SmartPointer.hpp"
