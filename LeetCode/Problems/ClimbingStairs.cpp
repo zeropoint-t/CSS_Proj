cd34a0ba9e0d35025aafca749946519e5d6266be
Tetsuya Hayashi (Teleion Consulting LLC) <v-thaya@microsoft.com>
2018-12-03 23:01:26 -0800

    LeetCode ClimbStairs revised

--- LeetCode/Problems/ClimbingStairs.cpp
+++ LeetCode/Problems/ClimbingStairs.cpp
@@ -1,8 +1,6 @@
-
 // 70. Climbing Stairs
 
 // Easy
-// 151258
 
 // You are climbing a stair case. It takes n steps to reach to the top.
 
@@ -25,20 +23,53 @@
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
 
-int climbing_Stairs(int i, int steps){
-    if(i < steps)
-        return climbing_Stairs(i+1, steps) + climbing_Stairs(i+2, steps);
-    
-    if(i = steps)
-        return 1;
+#include<map>
 
-    return 0;
-}
+using namespace std;
 
-int climbingStairs(int steps){
-    return climbing_Stairs(0,steps);
-}
+class ClimbingStairs{
+    public:
+        int climbing_Stairs(int cummulativeSteps, int numOfSteps){
+            if(cummulativeSteps < numOfSteps){
+
+                auto it1 = m.find(cummulativeSteps+1);
+                auto it2 = m.find(cummulativeSteps+2);
+                if(it1 != m.end() && it2 != m.end()){
+                    return it1->second + it2->second;
+                }else if(it1 != m.end() && it2 == m.end()){
+                    int i = climbing_Stairs(cummulativeSteps+2, numOfSteps);
+                    m.insert(pair<int,int>(cummulativeSteps+2,i));
+                    return it1->second + i;
+                }else if(it1 == m.end() && it2 != m.end()){
+                    int i = climbing_Stairs(cummulativeSteps+1, numOfSteps);
+                    m.insert(pair<int,int>(cummulativeSteps+1,i));
+                    return i + it2->second;
+                }else{
+                    int i1 = climbing_Stairs(cummulativeSteps+1, numOfSteps);
+                    m.insert(pair<int,int>(cummulativeSteps+1,i1));
+                    int i2 = climbing_Stairs(cummulativeSteps+2, numOfSteps);
+                    m.insert(pair<int,int>(cummulativeSteps+2,i2));
+                    return i1 + i2;
+                }
+            }
+            
+            if(cummulativeSteps == numOfSteps)
+                return 1;
+
+            if(cummulativeSteps > numOfSteps)
+                return 0;
+
+            return 0;
+        }
+
+        int climbingStairs(int numOfSteps){
+            return climbing_Stairs(0,numOfSteps);
+        }
+    private:
+        map<int,int> m;
+};
 
 int main(){
-    int a = climbingStairs(4);
+    ClimbingStairs cs;
+    int a = cs.climbingStairs(6);
 }
