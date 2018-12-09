35655df05d49e0830724d19a97f88794951f0867
zeropoint-t <tetsuyaeinstein@gmail.com>
2018-12-08 17:07:43 -0800

    HW8 completed

--- CSS501A/HW8/main.cpp
+++ CSS501A/HW8/main.cpp
@@ -1,11 +1,4 @@
 
-                // "${workspaceFolder}/CSS501A/HW8/main.cpp", 
-                // "${workspaceFolder}/CSS501A/HW8/Person.cpp", 
-                // "${workspaceFolder}/CSS501A/HW8/Instructor.cpp", 
-                // "${workspaceFolder}/CSS501A/HW8/Student.cpp", 
-                // "${workspaceFolder}/CSS501A/HW8/Supervisor.cpp", 
-                // "${workspaceFolder}/CSS501A/HW8/SkiSchool.cpp", 
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -26,52 +19,55 @@ int main(){
     school.registerInstructor("Instructor A");
     school.registerInstructor("Instructor B");
     school.registerInstructor("Instructor C");
-    school.registerInstructor("Instructor D");
 
     //fill all slots up to 5 students per instructor
-    for(int i = 1; i <= 20; i++){
+    for(int i = 1; i <= 19; i++){
         string student = "Student " + to_string(i);
         school.registerStudent(student);
     }
 
-    cout << "-----20 students are added-----" << endl;
+    cout << "-----19 students are added-----" << endl;
     school.displayClasses();
     cout << endl;
 
     //remove a instructor here
-    school.quitInstructor("Instructor D");
+    school.quitInstructor("Instructor C");
 
-    cout << "-----Instructor D was removed-----" << endl;
+    cout << "-----Instructor C was removed-----" << endl;
     school.displayClasses();
     cout << endl;
 
-    //remove 5 students
-    for(int i = 1; i <= 5; i++){
-        string student = "Student " + to_string(i);
-        school.quitStudent(student);
-    }
-
-    cout << "-----Top 5 students are removed-----" << endl;
-    school.displayClasses();
-    cout << endl;
+    cout << "-----Try to add a student to Instructor C who already quit the school-----" << endl;
+    school.registerStudent("Student 20 - Instructor C","Instructor D");
 
 
-    school.registerStudent("Student 21 - Instructor A","Instructor A");
-    school.registerStudent("Student 22 - Instructor A","Instructor A");
-    school.registerStudent("Student 23 - Instructor A","Instructor A");
-    cout << "-----3 new students requested for Instructor A-----" << endl;
-    school.displayClasses();
-    cout << endl;
+    // //remove 5 students
+    // for(int i = 1; i <= 5; i++){
+    //     string student = "Student " + to_string(i);
+    //     school.quitStudent(student);
+    // }
 
-    //add 10 more students
-    for(int i = 24; i <= 33; i++){
-        string student = "Student " + to_string(i);
-        school.registerStudent(student);
-    }
-    cout << "-----10 new students are added from 24 through 33-----" << endl;
-    school.displayClasses();
-    cout << endl;
+    // cout << "-----Top 5 students are removed-----" << endl;
+    // school.displayClasses();
+    // cout << endl;
 
-    cout << "-----Try to add a student to Instructor D who already quit the school-----" << endl;
-    school.registerStudent("Student 34 - Instructor D","Instructor D");
+
+    // school.registerStudent("Student 21 - Instructor A","Instructor A");
+    // school.registerStudent("Student 22 - Instructor A","Instructor A");
+    // school.registerStudent("Student 23 - Instructor A","Instructor A");
+    // cout << "-----3 new students requested for Instructor A-----" << endl;
+    // school.displayClasses();
+    // cout << endl;
+
+    // //add 10 more students
+    // for(int i = 24; i <= 33; i++){
+    //     string student = "Student " + to_string(i);
+    //     school.registerStudent(student);
+    // }
+    // cout << "-----10 new students are added from 24 through 33-----" << endl;
+    // school.displayClasses();
+    // cout << endl;
+
+    // cout << "-----Try to add a student to Instructor D who already quit the school-----" << endl;
+    // school.registerStudent("Student 34 - Instructor D","Instructor D");
 }
